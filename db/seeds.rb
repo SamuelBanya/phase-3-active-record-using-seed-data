569c3a4b84c59d5a771847594e01f83e772b738d
Samuel Banya <sbanya@fastmail.com>
2022-09-02 20:25:42 -0400

    Adding latest changes to assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,20 @@
-# Add a console message so we can see output when the seed file runs
+# db/seeds.rb
+# NOTE: This is what I had previously BEFORE I used 'Faker' to generate fake data:
+# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
+
+# Adding 'Faker' data:
+puts "Seeding games..."
+
+50.times do
+  Game.create(
+    title: Faker::Game.title,
+    genre: Faker::Game.genre,
+    platform: Faker::Game.platform,
+    price: rand(0..60) # Random number between 0 and 60
+  )
+end
+
+puts "Done seeding!"
